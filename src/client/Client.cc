5bdfa539d66e824318c329168297703cda78ee51
Dhairya Parmar <dparmar@redhat.com>
2024-08-21 17:54:16 +0530

    client: clamp sizes to INT_MAX in sync i/o code paths
    Fixes: https://tracker.ceph.com/issues/67612
    Signed-off-by: Dhairya Parmar <dparmar@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -15982,7 +15982,7 @@ int64_t Client::ll_writev(struct Fh *fh, const struct iovec *iov, int iovcnt, in
   }
 
   std::scoped_lock cl(client_lock);
-  return _preadv_pwritev_locked(fh, iov, iovcnt, off, true, false);
+  return _preadv_pwritev_locked(fh, iov, iovcnt, off, true, true);
 }
 
 int64_t Client::ll_readv(struct Fh *fh, const struct iovec *iov, int iovcnt, int64_t off)
@@ -15998,7 +15998,7 @@ int64_t Client::ll_readv(struct Fh *fh, const struct iovec *iov, int iovcnt, int
   }
 
   std::scoped_lock cl(client_lock);
-  return _preadv_pwritev_locked(fh, iov, iovcnt, off, false, false);
+  return _preadv_pwritev_locked(fh, iov, iovcnt, off, false, true);
 }
 
 int64_t Client::ll_preadv_pwritev(struct Fh *fh, const struct iovec *iov,
